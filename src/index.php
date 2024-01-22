71ba240c9a6811a16e78ecc5465bb1906539139f
mdorizon <mdorizon17@gmail.com>
2024-01-22 14:54:13 +0100

    db

--- src/index.php
+++ src/index.php
@@ -1,1 +1,21 @@
-<h1>Hello World</h1>
+<?php require_once 'parts/header.php'; ?>
+
+<section id="form-post" class="container mt-5">
+  <form action="scripts/post-create-script.php" method="POST">
+    <div class="mb-3">
+      <input type="text" class="form-control" placeholder="Title">
+    </div>
+    <input type="submit" value="Envoyer">
+  </form>
+</section>
+
+<section id="post-list" class="container mt-5">
+  <ul>
+    <li>Lorem, ipsum dolor.</li>
+    <li>Enim, corrupti veniam!</li>
+    <li>Fugit, doloribus voluptatem!</li>
+    <li>Laboriosam, voluptas fugit.</li>
+  </ul>
+</section>
+
+<?php require_once 'parts/footer.php'; ?>
---
+++ src/scripts/post-create-script.php
@@ -0,0 +1,4 @@
+<?php
+var_dump($_POST);
+$title_post = $_POST['title'];
+var_dump($title_post);

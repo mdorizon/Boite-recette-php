0d5ed63a91724001eec7f7e05367f890b3a32410
mdorizon <mdorizon17@gmail.com>
2024-01-22 16:30:17 +0100

    db connection wip

--- src/scripts/post-create-script.php
+++ src/scripts/post-create-script.php
@@ -4,3 +4,12 @@ $title_post = $_POST['title'];
 if(empty($title_post)) {
   header("Location: ../index.php?error=Veuillez renseigner un titre");
 }
+
+// connect to db with PDO
+$connectDatabase = new PDO("mysql:host=localhost;dbname=wordpress", "root", "admin");
+// prepare request
+$request = $connectDatabase->prepare("INSERT INTO posts (title) VALUES (:title)");
+// bind params
+$request->bindParam(':title', $title_post);
+// execute request
+$request->execute();

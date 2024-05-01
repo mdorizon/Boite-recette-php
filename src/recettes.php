94e403b17d1341528b4de50262b75175b3282987
mdorizon <mdorizon17@gmail.com>
2024-05-01 16:17:36 +0200

    :sparkles: add a recette if recette = 0

--- src/recettes.php
+++ src/recettes.php
@@ -24,25 +24,31 @@ $result = $request->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!-- show data in html -->
 <section>
-  <?php foreach ($result as $recette) : ?>
-    <?php
-      // liste des ingredients
-      $ingredients = preg_split('/,\s*/', $recette['ingredients']);
-      // liste des étapes
-      $steps = preg_split('/,\s*/', $recette['steps']);
-      // source de l'image
-      $img_src = $recette['img'];
-    ?>
-    <img src="<?= $img_src ?>" alt="recette<?= $recette['id']?>">
-    <p>Liste des ingrédients :</p>
-    <ul>
-      <?php foreach($ingredients as $ingredient){ echo('<li>' . $ingredient . '</li>'); } ?>
-    </ul>
-    <p>Etapes :</p>
-    <ol type="1">
-      <?php foreach($steps as $step){ echo('<li>' . $step . '</li>'); } ?>
-    </ol>
-  <?php endforeach; ?>
+  <?php if (count($result) == 0) : ?>
+    <div class="text-center">
+      <h2>Bonjour <?= $_SESSION['name']; ?>, Soyez le premier à poster une recette</h2>
+      <button>Ajouter</button>
+    </div>
+  <?php else: ?>
+    <?php foreach ($result as $recette) : ?>
+      <?php
+        // liste des ingredients
+        $ingredients = preg_split('/;\s*/', $recette['ingredients']);
+        // liste des étapes
+        $steps = preg_split('/;\s*/', $recette['steps']);
+      ?>
+      <img src="<?= $recette['img'] ?>" alt="recette<?= $recette['id']?>">
+      <h2><?= $recette['name'] ?></h2>
+      <p>Liste des ingrédients :</p>
+      <ul>
+        <?php foreach($ingredients as $ingredient){ echo('<li>' . $ingredient . '</li>'); } ?>
+      </ul>
+      <p>Etapes :</p>
+      <ol type="1">
+        <?php foreach($steps as $step){ echo('<li>' . $step . '</li>'); } ?>
+      </ol>
+    <?php endforeach; ?>
+  <?php endif; ?>
 </section>
 
 <?php require_once 'parts/footer.php'; ?>
